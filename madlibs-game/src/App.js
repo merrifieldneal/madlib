e2cd9de3a5eac0ac7a2a34a0b53488c5dc404800
Rusty Shackleford <rustyshackleford@MemeMachine>
2023-07-24 01:10:16 -0400

    added bonus requirements

--- madlibs-game/src/App.js
+++ madlibs-game/src/App.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 import './App.css';
 
+const storyOptions = [
+  {
+    title: 'Adventure',
+    story: 'Once upon a time, there was a brave {adjective} {noun} who decided to {verb} on a dangerous quest.',
+  },
+  {
+    title: 'Mystery',
+    story: 'In a small {adjective} town, a curious detective found a mysterious {noun} and set out to {verb} the case.',
+  },
+  {
+    title: 'Romance',
+    story: 'In a land far away, a {noun} and a {noun} fell in love and vowed to {verb} each other forever.',
+  },
+];
+
 const App = () => {
+  const [selectedStory, setSelectedStory] = useState('');
   const [words, setWords] = useState({
     noun: '',
     adjective: '',
@@ -18,48 +34,83 @@ const App = () => {
 
   const handleGenerateStory = () => {
     const { noun, adjective, verb } = words;
-    const story = `Once upon a time, there was a ${adjective} ${noun} who loved to ${verb} all day long.`;
+    const story = selectedStory
+      ? selectedStory.story.replace(/{(\w+)}/g, (_, key) => words[key])
+      : '';
     return story;
+  };
+
+  const handleRestart = () => {
+    setSelectedStory('');
+    setWords({
+      noun: '',
+      adjective: '',
+      verb: '',
+    });
   };
 
   return (
     <div className="App">
       <h1>Madlibs Game</h1>
-      <label>
-        Noun:
-        <input
-          type="text"
-          name="noun"
-          value={words.noun}
-          onChange={handleInputChange}
-        />
-      </label>
-      <br />
-      <label>
-        Adjective:
-        <input
-          type="text"
-          name="adjective"
-          value={words.adjective}
-          onChange={handleInputChange}
-        />
-      </label>
-      <br />
-      <label>
-        Verb:
-        <input
-          type="text"
-          name="verb"
-          value={words.verb}
-          onChange={handleInputChange}
-        />
-      </label>
-      <br />
-      <button onClick={handleGenerateStory}>Generate Story</button>
-      {words.noun && words.adjective && words.verb && (
+      {selectedStory ? (
         <div>
-          <h2>Story:</h2>
-          <p>{handleGenerateStory()}</p>
+          <h2>{selectedStory.title}</h2>
+          {words.noun && words.adjective && words.verb ? (
+            <div>
+              <h3>Story:</h3>
+              <p>{handleGenerateStory()}</p>
+              <button onClick={handleRestart}>Restart</button>
+            </div>
+          ) : (
+            <p>Please fill out all the prompts.</p>
+          )}
+        </div>
+      ) : (
+        <div>
+          <h2>Select a Story:</h2>
+          <ul>
+            {storyOptions.map((option, index) => (
+              <li key={index} onClick={() => setSelectedStory(option)}>
+                {option.title}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      {selectedStory && (
+        <div>
+          <h2>Fill in the Blanks:</h2>
+          <label>
+            Noun:
+            <input
+              type="text"
+              name="noun"
+              value={words.noun}
+              onChange={handleInputChange}
+            />
+          </label>
+          <br />
+          <label>
+            Adjective:
+            <input
+              type="text"
+              name="adjective"
+              value={words.adjective}
+              onChange={handleInputChange}
+            />
+          </label>
+          <br />
+          <label>
+            Verb:
+            <input
+              type="text"
+              name="verb"
+              value={words.verb}
+              onChange={handleInputChange}
+            />
+          </label>
+          <br />
+          <button onClick={handleGenerateStory}>Generate Story</button>
         </div>
       )}
     </div>
